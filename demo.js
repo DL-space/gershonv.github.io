82e95054ca44eb63b0877d646151c582958c7a06
gershonv <gershonv@163.com>
2018-11-20 23:27:01 +0800

    update

--- demo.js
+++ demo.js
@@ -1,46 +1,5 @@
-function fun(n, o) {
-  console.log(o)
-  return {
-    fun: function(m) {
-      return fun(m, n)
-    }
-  }
-}
+var arr = [1, 2, 3, 4]
 
-var a = fun(0)
-a.fun(1) 
-a.fun(2) 
-a.fun(3) 
-var b = fun(0)
-  .fun(1)
-  .fun(2)
-  .fun(3) 
-var c = fun(0).fun(1) 
-c.fun(2) 
-c.fun(3) 
+var cc = arr.toString().replace(/\d/g, 'x')
 
-function Foo() {
-  getName = function() {
-    console.log(1)
-  }
-  return this
-}
-Foo.getName = function() {
-  console.log(2)
-}
-Foo.prototype.getName = function() {
-  console.log(3)
-}
-var getName = function() {
-  console.log(4)
-}
-function getName() {
-  console.log(5)
-}
-
-Foo.getName() 
-getName()
-Foo().getName()
-getName()
-new Foo.getName() 
-new Foo().getName() 
+console.log(cc)
